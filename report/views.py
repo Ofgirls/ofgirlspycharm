95b14fe6e0d7b4a73063cbb3c3ab692dafc5421b
장보우 Bowoo Jang <codingbowoo@gmail.com>
2017-10-02 13:22:21 +0900

    Add files via upload
    Django app first commit

---
+++ report/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Report
+
+
+# Create your views here.
+def report_list(request):
+    # reports = Report.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    reports = Report.objects.all()
+    return render(request, 'report/report_list.html', {'reports': reports})
